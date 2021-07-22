ff15d514bc6e0e8a13c53266a864e5faf33411a5
DiegoFreitasDS <diegop.freitas@gmail.com>
2021-07-22 17:40:12 -0300

    updating projects

---
+++ JavaScrip/js/main.js
@@ -0,0 +1,7 @@
+var valor1 = int(25);
+var valor2 = int(100);
+var valor3 = float(5.50);
+var salary = float(valo1 * valor2).toFixed(valor3); // Digite aqui o calculo do salário
+
+console.log("NUMBER = "+ valor1);
+console.log("SALARY R$ "+ salary);
